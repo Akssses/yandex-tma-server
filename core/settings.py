94aee52782f4520de0e7595d34fd749c48fa837d
Akssses <itshnik07@gmail.com>
2025-10-22 11:52:05 +0600

    fixed

--- core/settings.py
+++ core/settings.py
@@ -134,3 +134,12 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CORS_ALLOW_CREDENTIALS = True
+
+# CSRF trusted origins
+CSRF_TRUSTED_ORIGINS = [
+    "https://yandextmaserver.ngrok.app",
+    "https://demisable-agueda-cloque.ngrok-free.dev",
+    "https://d0be66ba4906.ngrok-free.app",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
