9c0746100afe18bef2ddae82045b59e9d81cc975
Akssses <itshnik07@gmail.com>
2025-11-13 14:40:31 +0600

    new allowed host

--- core/settings.py
+++ core/settings.py
@@ -43,6 +43,8 @@ if _env_allowed_hosts:
 else:
     ALLOWED_HOSTS = [
         'metamarketing.muza.team',
+        'localhost',
+        '127.0.0.1',
     ]
 
 
@@ -165,6 +167,8 @@ if _env_cors:
 else:
     CORS_ALLOWED_ORIGINS = [
         "https://yandex-tma.vercel.app",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
@@ -176,6 +180,8 @@ if _env_csrf:
 else:
     CSRF_TRUSTED_ORIGINS = [
         "https://yandex-tma.vercel.app",
+        "http://localhost:8000",
+        "http://127.0.0.1:8000",
     ]
 
 # Django REST Framework settings
